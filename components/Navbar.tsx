713de2ddb4b5747a040e6757754d4b86c2ef72d1
Niki Tsolaki <nikitsolaki10@gmail.com>
2025-10-28 15:57:11 +0200

    Explore button, navbar.

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <Link href="/" className="logo">
+                    <Image src="/icons/logo.png" width={24} height={24} alt="logo" />
+                    <p>DevEvent</p>
+                </Link>
+
+                <ul className="nav">
+                    <Link href="/" className="nav-item">Home</Link>
+                    <Link href="/" className="nav-item">Events</Link>
+                    <Link href="/" className="nav-item">Create Event</Link>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Navbar
