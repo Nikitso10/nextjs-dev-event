5aa4ed632cb3af881e58230ca805365e5f4e9891
Niki Tsolaki <nikitsolaki10@gmail.com>
2025-11-03 00:15:31 +0200

    Caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 // Asynchronous server page allowing top level await
 const Page = async () => {
+    'use cache';
+    cacheLife('hours')
     const response = await fetch(`${BASE_URL}/api/events`);
     // destructure the actual events
     const {events} = await response.json();
