5bf214bd512bc7f355a2c48df4c6eeffc5d3772a
Niki Tsolaki <nikitsolaki10@gmail.com>
2025-11-03 23:16:41 +0200

    comply with v16

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
+import events from "@/lib/constants";
 
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -9,9 +10,9 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    const response = await fetch(`${BASE_URL}/api/events`);
+    // const response = await fetch(`${BASE_URL}/api/events`);
     // destructure the actual events
-    const {events} = await response.json();
+    // const {events} = await response.json();
 
     return (
         <section>
