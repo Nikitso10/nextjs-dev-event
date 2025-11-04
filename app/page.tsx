39886022fbb78d2a6e260773b5f72d6165e7ff5d
Niki Tsolaki <nikitsolaki10@gmail.com>
2025-11-04 22:33:01 +0200

    page fetch no-store cache removal

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const Page = async () => {
     'use cache';
     cacheLife('hours')
     const baseUrl = getBaseUrl();
-    const res = await fetch(`${baseUrl}/api/events`, { cache: 'no-store' });
+    const res = await fetch(`${baseUrl}/api/events`);
 
     // If the API fails, fail gracefully
     if (!res.ok) {
